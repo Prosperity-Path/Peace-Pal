805a1db91684da01592267ee2a614eef251f4c48
Sam Ellis <sam.dave.ellis@gmail.com>
2023-05-23 20:26:22 -0400

    schedule trigger upon hitting first message and then schedule new trigger every time a scheduled trigger hits.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from response_content import Content
 from emotion_model import PredictEmotion
+import datetime
 
 class Message(BaseModel):
     messageId: str
@@ -20,6 +21,9 @@ emotion_model = PredictEmotion()
 @app.post("/first-message")
 async def intro_reply(message: Message, request: Request):
     res_content = Content(message, request)
+    nowdt = datetime.datetime.now()
+    newdt = nowdt + datetime.timedelta(minutes=5)
+    res_content.schedule_trigger(newdt, message.sender)
     return Content.intro_message()
 
 @app.post("/new-message")
@@ -38,11 +42,16 @@ async def field_reply(reply: Reply, request: Request):
 
 @app.post("/scheduled")
 async def scheduled_reply(message: Message, request: Request):
+    # Message is coming from a trigger, so we need to re
     res_content = Content(message, request)
     res =  {
         "subject": "Daily Peace Exercise",
         "message":res_content.pick_scheduled_exercise(),
-        "to": message.message
+        "to": message.recipient
     }
+    # Schedule next message tomorrow
+    nowdt = datetime.datetime.now()
+    newdt = nowdt + datetime.timedelta(hours=24)
+    res_content.schedule_trigger(newdt, message.recipient)
     return res
 
